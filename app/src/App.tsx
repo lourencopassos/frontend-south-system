e37a4ad5cc92cc0765341ee2321b37410eeebdd5
Lourenco <lo.passos93@gmail.com>
2021-05-16 19:24:38 -0300

    chore: CRA project init

---
+++ app/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./styles/style.scss";
+import Login from "./pages/Login/login.index";
+import Router from "./router/router.index";
+
+function App() {
+  return (
+    <div className="App">
+      <Router />
+    </div>
+  );
+}
+
+export default App;
